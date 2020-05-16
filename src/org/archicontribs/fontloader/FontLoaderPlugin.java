722618b29ff6241074123ff057798032621b343a
JB Sarrodie <jean-baptiste@sarrodie.org>
2020-05-16 23:12:36 +0200

    Load fonts from builtin and user "fonts" folder

--- src/org/archicontribs/fontloader/FontLoaderPlugin.java
+++ src/org/archicontribs/fontloader/FontLoaderPlugin.java
@@ -12,16 +12,19 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+import com.archimatetool.editor.preferences.IPreferenceConstants;
+import com.archimatetool.editor.preferences.Preferences;
 
 
 /**
- * Activitor
+ * Activator
  * 
- * @author JB Sarrodie
+ * @author JB Sarrodie & Phil Beauvoir
  */
 @SuppressWarnings("nls")
 public class FontLoaderPlugin extends AbstractUIPlugin {
@@ -37,15 +40,53 @@ public class FontLoaderPlugin extends AbstractUIPlugin {
     }
     
     @Override
+    
     public void start(BundleContext context) throws Exception {
         super.start(context);
         
-        final String fontPath = "/tmp/fonts/Archi-FontAwesome.ttf";
-        
         Display.getDefault().asyncExec(() -> {
-            boolean success = Display.getDefault().loadFont(fontPath);
-            System.out.println(success ? "loaded" : "not loaded");
+            // Load fonts distributed with the plugin
+            loadFonts(getFontsFolder());
+            
+            // Load user fonts
+            loadFonts(getUserFontsFolder());
         });
+    }
+    
+    // Scan a folder looking for fonts and load them
+    private void loadFonts(File fontFolder) {
+    	Display display = Display.getDefault();
+    	
+        if(fontFolder.exists()) {
+            for(File font : fontFolder.listFiles()) {
+                if(font.isFile()) {
+                	display.loadFont(font.getAbsolutePath());
+                }
+            }
+        }
+    }
+    
+    /**
+     * @return User fonts folder
+     */
+    public File getUserFontsFolder() {
+        File folder = new File(Preferences.STORE.getString(IPreferenceConstants.USER_DATA_FOLDER), "fonts"); //$NON-NLS-1$
+        folder.mkdirs();
+        return folder;
+    }
+    
+    /**
+     * @return The plugin fonts folder
+     */
+    public File getFontsFolder() {
+        URL url = FileLocator.find(getBundle(), new Path("$nl$/fonts"), null); //$NON-NLS-1$
+        try {
+            url = FileLocator.resolve(url);
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+        }
+        return new File(url.getPath()); 
     }
     
     /**
